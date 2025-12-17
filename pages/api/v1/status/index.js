b746d84c82fc6d6f6f08c53068440ff356b03179
Carlos <darcilemons@gmail.com>
2025-12-17 08:19:16 -0300

    endpoint `api/v1/status` working great

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,30 @@
 import database from "infra/database.js";
 
-async function status(req, res) {
+export default async function status(req, res) {
   const updatedAt = new Date().toISOString();
+  
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseMaxConnectionsResult = await database.query("SHOW max_connections;")
+  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*) FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName]
+  });
+  const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
+  
 
   res.status(200).json({
     updated_at: updatedAt,
-  });
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        database_name: databaseName,
+        max_connections: databaseMaxConnectionsValue,
+        opened_connections: databaseOpenedConnectionsValue
+      }
+    }
+  })
 }
 
-export default status;
